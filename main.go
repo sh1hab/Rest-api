0e8f67cd5fba8b1e3adead6c61d856ef2943eb9e
sh1hab <shihab.gsc10@gmail.com>
2020-05-04 00:18:43 +0600

    struct fix

--- main.go
+++ main.go
@@ -4,22 +4,26 @@ import (
 		"fmt"
 		"net/http"
 		"log"
-		
+		"encoding/json"		
 )
 
+// Contact is 
 type Contact struct{
-	email string `json:"Email"`
-	password string `json:Password`
-	status string `json:status`
+	Email string `json:"Email"`
+	Password string `json:"Password"`
+	Status string `json:"status"`
 }
 
-var Contacts []Contact
+// Contacts is
+type Contacts []Contact
 
 func main(){
+	fmt.Println("Program is started...")
 	handleRequests()
 }
 
 func handleRequests(){
+	fmt.Println("Server is listening to requests in 127.0.0.1:8080 ")
 	http.HandleFunc("/",homePage)
 	http.HandleFunc("/contacts",allContacts)
 	log.Fatal(http.ListenAndServe(":8080",nil) )
@@ -31,7 +35,7 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func allContacts(w http.ResponseWriter, r *http.Request){
 	contacts := Contacts{
-		Contact{ "email":"abc@example.com","password":"123","status":"active" },
+		Contact{ Email:"abc@example.com",Password:"123",Status:"active" },
 	} 
 	json.NewEncoder(w).Encode( contacts )
 }
